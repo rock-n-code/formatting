6e7ca76f5b04f40dcc313bb4ad171d5a7d92608a
Javier Cicchelli <javier@rock-n-code.com>
2022-10-27 23:10:40 +0200

    Defined the Runnable protocol.

---
+++ Sources/Formatting/Protocols/Runnable.swift
@@ -0,0 +1,21 @@
+//
+//  Runnable.swift
+//  FormattingCore
+//
+//  Created by Javier Cicchelli on 27/10/2022.
+//  Copyright © 2022 Röck+Cöde. All rights reserved.
+//
+
+import Foundation
+
+protocol Runnable {
+    
+    // MARK: Functions
+    
+    @discardableResult
+    func callAsFunction(
+        pathToCommand: String,
+        arguments: [String]?
+    ) throws -> Data?
+    
+}
